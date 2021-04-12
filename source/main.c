e35e50e215e23d8c1f0735344473aede32a8c244
Kalyan Sunkara <ksunk001@wch136-24.cs.ucr.edu>
2021-04-11 19:58:37 -0700

    pre save

--- source/main.c
+++ source/main.c
@@ -19,54 +19,14 @@ int main(void) {
 	PORTA = 0xFF;
 	DDRC = 0xFF;
 	PORTC = 0x00;
+	DDRB = 0xFF;
+        PORTB = 0x00;
     /* Insert your solution below */
-//	unsigned char total = 0;
-	unsigned char tmpA = 0;
-	unsigned char tmpA4 = 0; //ignition
-	unsigned char tmpA5 = 0; //seated
-	unsigned char tmpA6 = 0; //seatbelt 
+//	unsigned char total = 0; 
     while (1) {
-	tmpA = PINA & 0x0F;
-	tmpA4 = (PINA & 0x10) >> 3;
-	tmpA5 = (PINA & 0x20) >> 4;
-	tmpA6 = (PINA & 0x40) >> 5;
-	if(tmpA < 3){
-		PORTC = 0x60;
-		if(!tmpA6 && tmpA5 && tmpA4){
-			PORTC = PORTC | 0x80;
-		}
-	}
-	else if(tmpA < 5){
-		PORTC = 0x70;
-		if(!tmpA6 && tmpA5 && tmpA4){
-                        PORTC = PORTC | 0x80;
-                }
-	}
-	else if(tmpA < 7){
-                PORTC = 0x38;
-		if(!tmpA6 && tmpA5 && tmpA4){
-                        PORTC = PORTC | 0x80;
-                }
-        }
-	else if(tmpA < 10){
-		PORTC = 0x3C;
-		if(!tmpA6 && tmpA5 && tmpA4){
-                        PORTC = PORTC | 0x80;
-                }
-	}
-	else if(tmpA < 13){
-		PORTC = 0x3E;
-		if(!tmpA6 && tmpA5 && tmpA4){
-                        PORTC = PORTC | 0x80;
-                }
-	}
-	else{
-		PORTC = 0x3F;
-		if(!tmpA6 && tmpA5 && tmpA4){
-                        PORTC = PORTC | 0x80;
-                }
-	}		
-}	
+	PORTB = ((PINA & 0xF0) >> 4);
+	PORTC = ((PINA & 0x0F) << 4);   
+	}	
     return 1;
 }
 
